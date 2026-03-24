967980282ec7165cf471cf89414aab5f53f95951
Angie <angiepenaalvarez@gmail.com>
2026-03-23 22:36:38 -0500

    Directiva de validación de errores de formularios (Issue #24) (#53)
    * Creacion de directiva de validacion de errores de formulario
    
    * Detalles corregidos
    
    * Implementacion validación del minlenght

--- src/app/pages/auth/register/register.module.ts
+++ src/app/pages/auth/register/register.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
 import { AuthThemeModule } from 'src/app/core/themes/auth/auth-primeng.module';
+import { SharedDirectiveModule } from "src/app/shared/directive/shared-directive.module";
 
 @NgModule({
   declarations: [RegisterComponent],
   imports: [
     RegisterRoutingModule,
     AuthThemeModule,
+    SharedDirectiveModule,
   ],
 })
 export class RegisterModule {}
--- src/app/shared/directive/shared-directive.module.ts
+++ src/app/shared/directive/shared-directive.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ActivoInactivoDirective } from './activo-inactivo/activo-inactivo.directive';
 import { BooleanStatusDirective } from './activo-inactivo/boolean-status.directive';
+import { ErrorFormDirective } from './error-form/error-form-directive';
 
 @NgModule({
-  declarations: [ActivoInactivoDirective, BooleanStatusDirective],
-  exports: [ActivoInactivoDirective, BooleanStatusDirective],
+  declarations: [ActivoInactivoDirective, BooleanStatusDirective, ErrorFormDirective],
+  exports: [ActivoInactivoDirective, BooleanStatusDirective, ErrorFormDirective],
 })
 export class SharedDirectiveModule {}
