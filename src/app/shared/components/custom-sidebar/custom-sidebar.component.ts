a77cfc13aeb691564fea287410d548920f7c1400
Shol551 <sholanshsuarez@gmail.com>
2026-05-13 20:52:06 -0500

    Feature/44/implementar diseño ver perfil (#65)
    * feat: implementar vista de perfil de usuario
    
    * feature/44/Implementar-diseño-ver-perfil
    
    * fix: actualización de avatar en tiempo real
    
    * feature/44/Implementar-diseño-ver-perfil - 05/05/2026

--- src/app/shared/components/custom-sidebar/custom-sidebar.component.ts
+++ src/app/shared/components/custom-sidebar/custom-sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
 import { Menu } from '../../class/Menu.class';
 import { IMenu } from 'src/app/core/interfaces/interfaces';
 
@@ -13,7 +13,38 @@ export class CustomSidebarComponent {
 
   active: boolean = false;
 
+  sidebarVisible: boolean = false;
+  isMobile: boolean = false;
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth < 768;
+    if (!this.isMobile) {
+      this.sidebarVisible = true;
+    } else {
+      this.sidebarVisible = false;
+    }
+  }
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
+
+  closeSidebar() {
+    this.sidebarVisible = false;
+  }
+  
   toggleTheme() {
     this.active = !this.active;
   }
+
+  
 }
